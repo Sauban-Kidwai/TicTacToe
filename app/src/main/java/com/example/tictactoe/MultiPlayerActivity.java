39b9c2a969753ee1c52e60e88b5acd21ebb8cc9a
Sauban-Kidwai <saubanmehmood@gmail.com>
2023-09-19 22:57:03 +0800

    made the text in the buttons Black. Added Multiplayer functionality. User clicks on Multiplayer and sees the multiplayer settings screen

---
+++ app/src/main/java/com/example/tictactoe/MultiPlayerActivity.java
@@ -0,0 +1,62 @@
+package com.example.tictactoe;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MultiPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_settings_screen_singleplayer);
+
+        int orientation = getResources().getConfiguration().orientation;
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // Load the landscape layout
+            setContentView(R.layout.fragment_settings_screen_2player_land);
+        } else {
+            // Load the portrait layout
+            setContentView(R.layout.fragment_settings_screen_2player);
+        }
+
+        // Find the 3x3 button by its ID
+        Button three = findViewById(R.id.board_3x3_button);
+        Button four = findViewById(R.id.board_4x4_button);
+        Button five = findViewById(R.id.board_5x5_button);
+
+        // OnClickListener button
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // When the button is clicked, start the new activity
+                Intent intent = new Intent(MultiPlayerActivity.this, ThreeByThree.class);
+                startActivity(intent);
+            }
+        });
+
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // When the button is clicked, start the new activity
+                Intent intent = new Intent(MultiPlayerActivity.this, FourByFour.class);
+                startActivity(intent);
+            }
+        });
+
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // When the button is clicked, start the new activity
+                Intent intent = new Intent(MultiPlayerActivity.this, FiveByFive.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
