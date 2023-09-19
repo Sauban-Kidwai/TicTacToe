4e6c9a7d005b23efd82ea94a1655514c76079677
Sauban-Kidwai <saubanmehmood@gmail.com>
2023-09-19 21:43:02 +0800

    added settings screen when in either single or multiplayer. did portrait and landscape orientation.

--- app/src/main/java/com/example/tictactoe/SinglePlayerActivity.java
+++ app/src/main/java/com/example/tictactoe/SinglePlayerActivity.java
@@ -17,15 +17,15 @@ public class SinglePlayerActivity extends AppCompatActivity {
         setContentView(R.layout.fragment_game_board_3x3);
 
         buttons = new Button[9];
-        buttons[1] = findViewById(R.id.Button1);
-        buttons[2] = findViewById(R.id.Button2);
-        buttons[3] = findViewById(R.id.Button3);
-        buttons[4] = findViewById(R.id.Button4);
-        buttons[5] = findViewById(R.id.Button5);
-        buttons[6] = findViewById(R.id.Button6);
-        buttons[7] = findViewById(R.id.Button7);
-        buttons[8] = findViewById(R.id.Button8);
-        buttons[9] = findViewById(R.id.Button9);
+        buttons[0] = findViewById(R.id.Button1);
+        buttons[1] = findViewById(R.id.Button2);
+        buttons[2] = findViewById(R.id.Button3);
+        buttons[3] = findViewById(R.id.Button4);
+        buttons[4] = findViewById(R.id.Button5);
+        buttons[5] = findViewById(R.id.Button6);
+        buttons[6] = findViewById(R.id.Button7);
+        buttons[7] = findViewById(R.id.Button8);
+        buttons[8] = findViewById(R.id.Button9);
 
     }
 }
