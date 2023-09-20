37d5dbfe67cce4216739d2f975d134d69fc57866
Sauban-Kidwai <saubanmehmood@gmail.com>
2023-09-20 16:38:44 +0800

    added timer functionality currently set to 1 minute down

--- app/src/main/java/com/example/tictactoe/MultiPlayerActivity.java
+++ app/src/main/java/com/example/tictactoe/MultiPlayerActivity.java
@@ -49,6 +49,7 @@ public class MultiPlayerActivity extends AppCompatActivity {
             }
         });
 
+        //five by five
         five.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
