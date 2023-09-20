4c4305f44464b84ad98133b5f367f0243c3e4eb7
littsen <littsen.ong95@gmail.com>
2023-09-20 18:06:28 +0800

    multiplayer game works

--- app/src/main/java/com/example/tictactoe/ThreeByThreeMulti.java
+++ app/src/main/java/com/example/tictactoe/ThreeByThreeMulti.java
@@ -9,6 +9,8 @@ package com.example.tictactoe;
     import android.widget.Button;
 
 public class ThreeByThreeMulti extends AppCompatActivity {
+    private char[][] board; // 3x3 Tic-Tac-Toe board
+    private char currentPlayer; // Current player (either 'X' or 'O')
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +25,87 @@ public class ThreeByThreeMulti extends AppCompatActivity {
             setContentView(R.layout.fragment_game_board_3x3);
         }
 
-        // Add more buttons and logic for the rest of the grid cells...
+        // Initialize the game board
+        board = new char[3][3];
+        currentPlayer = getIntent().getCharExtra("playerOneElement", 'X'); // Start with player one's marker
+
+        // Initialize the buttons for each grid cell and set click listeners
+        Button[][] buttons = new Button[3][3];
+        buttons[0][0] = findViewById(R.id.Button1);
+        buttons[0][1] = findViewById(R.id.Button2);
+        buttons[0][2] = findViewById(R.id.Button3);
+        buttons[1][0] = findViewById(R.id.Button4);
+        buttons[1][1] = findViewById(R.id.Button5);
+        buttons[1][2] = findViewById(R.id.Button6);
+        buttons[2][0] = findViewById(R.id.Button7);
+        buttons[2][1] = findViewById(R.id.Button8);
+        buttons[2][2] = findViewById(R.id.Button9);
+
+// Set click listeners for each grid cell
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                final int row = i;
+                final int col = j;
+                buttons[i][j].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Handle the player's move
+                        onCellClick(row, col, (Button) view);
+                    }
+                });
+            }
+        }
+    }
+    // Handle a player's move
+    private void onCellClick(int row, int col, Button button) {
+        if (board[row][col] == '\0') { // Check if the cell is empty
+            board[row][col] = currentPlayer;
+            button.setText(String.valueOf(currentPlayer));
+
+            // Check for a win or a draw
+            if (checkWin(row, col, currentPlayer)) {
+                // Handle the game result (player wins)
+                showGameResult(currentPlayer + " wins!");
+                disableAllButtons();
+            } else if (isBoardFull()) {
+                // Handle the game result (draw)
+                showGameResult("It's a draw!");
+            } else {
+                // Switch to the next player
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            }
+        }
+    }
+    // Check if the current player has won
+    private boolean checkWin(int row, int col, char player) {
+        // Implement your win condition logic here
+        // Check rows, columns, and diagonals
+        // Return true if the player has won, otherwise return false
+        // Example: check rows
+        return (board[row][0] == player && board[row][1] == player && board[row][2] == player);
+    }
+    // Check if the board is full (a draw)
+    private boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '\0') {
+                    return false; // There's an empty cell, the board is not full
+                }
+            }
+        }
+        return true; // All cells are filled, it's a draw
+    }
+    // Show the game result (you can customize this)
+    private void showGameResult(String message) {
+        // You can display the result in a TextView or customize it as per your UI
+        // For example:
+        // TextView resultTextView = findViewById(R.id.resultTextView);
+        // resultTextView.setText(message);
+    }
+
+    // Disable all grid cell buttons
+    private void disableAllButtons() {
+        // Disable all buttons to prevent further moves
+        // You can implement this based on your button IDs
     }
 }
